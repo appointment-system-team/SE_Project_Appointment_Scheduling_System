a3eb18ba410790adddcc6ddf950420c0d5e07fdf
zeina <zeina.anabtawi4@gmail.com>
2026-03-10 21:23:54 +0200

    Add JUnit5 and initial test

---
+++ src/test/java/com/appointments/AppTest.java
@@ -0,0 +1,11 @@
+package com.appointments;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+@Test
+    void basicTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
+
